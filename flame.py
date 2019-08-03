a4bc788d7f42c9eac79a91187e1802096bb07bc5
Channyein23 <uchannyein5@gmail.com>
2019-08-03 04:15:33 +0100

    DHT plus SMS

---
+++ flame.py
@@ -0,0 +1,14 @@
+import RPi.GPIO as GPIO
+import time
+
+channel = 21
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(channel, GPIO.IN)
+def callback(channel):
+	print("Flame detected")
+
+GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)
+GPIO.add_event_callback(channel, callback)
+
+while True:
+	time.sleep(1)
