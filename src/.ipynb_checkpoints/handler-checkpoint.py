a6093cfe024ce79a8d0fe0b70709770e2fc43a9a
Channyein23 <uchannyein5@gmail.com>
2019-08-03 12:42:11 +0630

    Debugging Chain

--- src/.ipynb_checkpoints/handler-checkpoint.py
+++ src/.ipynb_checkpoints/handler-checkpoint.py
@@ -1,6 +1,7 @@
 class AbstractHandler:
     def handle(self, request):
         try:
+            print(self._next)
             self._next.handle(request)
             print(request)
         except AttributeError:
