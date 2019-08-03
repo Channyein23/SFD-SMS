c5c43cac30044b205293ced2d46545b07995a891
Channyein23 <uchannyein5@gmail.com>
2019-08-03 11:39:38 +0630

    DHT pin

--- src/.ipynb_checkpoints/home-checkpoint.py
+++ src/.ipynb_checkpoints/home-checkpoint.py
@@ -4,7 +4,7 @@ import dth11
 import sms
 
 if __name__ == '__main__':
-    dht = dht11.DHT11(pin = )
+    dht = dht11.DHT11(pin = 4)
     fis = fuzzy.FIS.instance()
     dht.set_next(fis)
     dht.handle({})
