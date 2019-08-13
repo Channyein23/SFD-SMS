efc6c63bcbafe82a461c4182f4f7feff3e931de3
Channyein23 <uchannyein5@gmail.com>
2019-08-13 23:09:01 +0630

    change print

--- src/sendsms.py
+++ src/sendsms.py
@@ -19,34 +19,34 @@ port = serial.Serial("/dev/ttyAMA0", baudrate=9600, timeout=1)
 port.write('AT'+'\r\n')
 port.write("\x0D\x0A")
 rcv = port.read(10)
-print rcv
+print (rcv)
 time.sleep(1)
  
 port.write('ATE0'+'\r\n')                 # Disable the Echo
 rcv = port.read(10)
-print rcv
+print (rcv)
 time.sleep(1)
  
 port.write('AT+CMGF=1'+'\r\n')            # Select Message format as Text mode 
 rcv = port.read(10)
-print rcv
+print (rcv)
 time.sleep(1)
  
 port.write('AT+CNMI=2,1,0,0,0'+'\r\n')      # New SMS Message Indications
 rcv = port.read(10)
-print rcv
+print (rcv)
 time.sleep(1)
  
 ck=1
 while ck==1:
     rcv = port.read(10)
-    print rcv
+    print (rcv)
     fd=rcv
     if len(rcv)&gt;3:                   # check if any data received 
         ck=12
         for i in range(5):            
             rcv = port.read(10)
-            print rcv
+            print (rcv)
             fd=fd+rcv                 # Extract the complete data 
  
 # Extract the message number shown in between the characters "," and '\r'
@@ -61,5 +61,5 @@ while ck==1:
         for j in range(10):
             rcv = port.read(20)
             msg=msg+rcv
-        print msg
+        print (msg)
     time.sleep(0.1)
