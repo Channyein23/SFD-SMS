d116259623f285454788f298d22e2e53470ff295
Channyein23 <uchannyein5@gmail.com>
2019-08-22 15:35:07 +0630

    resolve

--- src/.ipynb_checkpoints/flame-checkpoint.py
+++ src/.ipynb_checkpoints/flame-checkpoint.py
@@ -9,3 +9,6 @@ GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)
 
 def register_callback(callback):
     GPIO.add_event_callback(channel, lambda channel: callback())
+    
+if __name__ = '__main__':
+    time.sleep(1000)
