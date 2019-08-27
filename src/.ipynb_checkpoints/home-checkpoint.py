6d16b6c8cabb46b3156cd0579cd4e0ce93209acf
Channyein23 <uchannyein5@gmail.com>
2019-08-27 21:25:06 +0630

    more sleep time

--- src/.ipynb_checkpoints/home-checkpoint.py
+++ src/.ipynb_checkpoints/home-checkpoint.py
@@ -7,13 +7,15 @@ import time
 dht = dht11.DHT11(pin = 4)
 fis = fuzzy.FIS.instance()
 sender = sms.SMS()
+
 dht.set_next(fis)
 fis.set_next(sender)
+
 def onFlame():
     dht.handle({})
-    time.sleep(50000)
+    time.sleep(1000)
 
 if __name__ == '__main__':
     flame.register_callback(onFlame)
     while True:
-        time.sleep(5)
+        time.sleep(5000)
--- src/home.py
+++ src/home.py
@@ -13,9 +13,9 @@ fis.set_next(sender)
 
 def onFlame():
     dht.handle({})
-    time.sleep(10)
+    time.sleep(1000)
 
 if __name__ == '__main__':
     flame.register_callback(onFlame)
     while True:
-        time.sleep(5)
+        time.sleep(5000)
