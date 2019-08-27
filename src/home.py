210b112952b269e442b4d2878658c0d1731c10c6
Channyein23 <uchannyein5@gmail.com>
2019-08-27 22:08:31 +0630

    change sleep time

--- src/home.py
+++ src/home.py
@@ -13,9 +13,9 @@ fis.set_next(sender)
 
 def onFlame():
     dht.handle({})
-    time.sleep(1000)
+    time.sleep(10)
 
 if __name__ == '__main__':
     flame.register_callback(onFlame)
     while True:
-        time.sleep(5000)
+        time.sleep(5)
