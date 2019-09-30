bcc1f32d3aac3525ca901825e5bef97aa558805c
Channyein23 <uchannyein5@gmail.com>
2019-10-01 00:06:46 +0630

    delet sms

--- src/testsms.py
+++ src/testsms.py
@@ -42,37 +42,4 @@ port.write("\x1A".encode('utf-8'))# Enable to send SMS
 for i in range(10):
     rcv = port.read(10)
     print (rcv)
-port.write('AT\r\n'.encode('utf-8'))
-rcv = port.read(10)
-print (rcv)
-time.sleep(1)
-
-port.write('ATEO\r\n'.encode('utf-8'))
-rcv = port.read(10)
-print (rcv)
-time.sleep(1)
-
-port.write('AT+CMGF=1\r\n'.encode('utf-8'))
-rcv = port.read(10)
-print (rcv)
-time.sleep(1)
-
-port.write('AT+CNMI=2,1,0,0,0\r\n'.encode('utf-8'))
-rcv = port.read(10)
-print (rcv)
-time.sleep(1)
-
-port.write('AT+CMGS="+09261002643"\r\n'.encode('utf-8'))
-rcv = port.read(10)
-print (rcv)
-time.sleep(1)
-
-port.write('Hello User\r\n'.encode('utf-8'))
-rcv = port.read(10)
-print (rcv)
-
-port.write("\x1A".encode('utf-8'))
-for i in range(10):
-	rcv = port.read(10)
-	print (rcv)
 
